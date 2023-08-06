001e802c3229b5b53119dfc817a4fd7e436510be
KarthikHD <KarthikBharadwaj.HD@ellucian.com>
2023-08-06 18:00:02 +0530

    Updated fileuploader function

---
+++ fileUploader/index.js
@@ -0,0 +1,44 @@
+//using multer for uploading files
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// Set 'views' directory for EJS templates
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set up multer for file upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+// Serve the EJS form for file upload
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Handle the file upload
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({ message: 'File uploaded successfully', filename: req.file.filename });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
